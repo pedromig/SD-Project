f5c67b52e7ef9417f67344fb0b913fc12c53f496
MikeLrUC <rabuge@student.dei.uc.pt>
2021-04-05 05:28:40 +0100

    Rmi Client Interfaces javadoc done

--- src/rmi/interfaces/RmiAdminConsoleInterface.java
+++ src/rmi/interfaces/RmiAdminConsoleInterface.java
@@ -3,12 +3,25 @@ package rmi.interfaces;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * An RMI Administrator Console Client Interface
+ */
 public interface RmiAdminConsoleInterface extends Remote {
 
+    /**
+     * Remote Print
+     * @param msg String to be printed in the server
+     * @throws RemoteException
+     */
     default void print(String msg) throws RemoteException {
         System.out.println(msg);
     }
 
+    /**
+     * A remote getter implementation for the realTimeElectionName attribute
+     * @return this realTimeElectionName
+     * @throws RemoteException
+     */
     String getRealTimeElectionName() throws RemoteException;
 
 }
